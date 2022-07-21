247b2e03c6353bc3d2838d57c0ecdb86561e6619
bemuron <emo6bruno@gmail.com>
2022-07-21 17:11:54 +0300

    add initial commit

---
+++ database/migrations/2022_07_21_130247_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id',false);
+            $table->string('file_name');
+            $table->string('content_type');
+            $table->string('file_size');
+            $table->text('note')->nullable();
+            $table->binary('file_data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
